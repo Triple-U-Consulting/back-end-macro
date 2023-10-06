5848b1cc0b5b7458d66018b28a8d3bde396a8bca
GeraldyK <76173300+GeraldyK@users.noreply.github.com>
2023-10-06 14:17:27 +0700

    add remote database

--- database/database.js
+++ database/database.js
@@ -1,7 +1,9 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
+const connectionString = process.env.DB_URL;
 
 // connect to database
+/* connect to local database
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -11,6 +13,12 @@ const pool = new Pool({
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000
+});
+*/
+
+// connect to remote database
+const pool = new Pool({
+    connectionString,
 });
 
 module.exports = pool
