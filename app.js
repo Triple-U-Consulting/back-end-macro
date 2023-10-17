b3db6b340745af6f93157bcb381c007547779bc3
GeraldyK <76173300+GeraldyK@users.noreply.github.com>
2023-10-17 15:20:07 +0700

    update command

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(cookieParser());
-//asdasdas
+// tes git
 // Header
 // app.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "*");
