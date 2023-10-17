31e9051d66ed6d706c71311d05bda88d7ea01e70
Angelica Patricia Djaya Saputra <121279021+angelpatriciads@users.noreply.github.com>
2023-10-17 14:34:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 // import package
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
-const kambuhRouter = require('./api/router/kambuhRouter');
-const authRouter = require('./api/router/authRouter');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-require('dotenv').config();
+const kambuhRouter = require("./api/router/kambuhRouter");
+const authRouter = require("./api/router/authRouter");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config();
 
 //middlewate
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(cookieParser());
-
+//asdasdas
 // Header
 // app.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "*");
@@ -28,26 +28,26 @@ app.use(cookieParser());
 // });
 
 //router
-app.use('/kambuh', kambuhRouter);
-app.use('/auth', authRouter);
+app.use("/kambuh", kambuhRouter);
+app.use("/auth", authRouter);
 
 // error handling
 // declare error
 app.use((req, res, next) => {
-    const error = new Error
-    error.status = 404
+  const error = new Error();
+  error.status = 404;
 
-    // pass error as a parameter
-    next(error)
+  // pass error as a parameter
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500).json({
-        error: {
-            message: 'Not Found'
-        }
-    })
-    console.log(error)
-})
+  res.status(error.status || 500).json({
+    error: {
+      message: "Not Found",
+    },
+  });
+  console.log(error);
+});
 
 module.exports = app;
