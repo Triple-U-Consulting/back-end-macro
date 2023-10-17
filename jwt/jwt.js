cc7c92186cc4d708870db749f22705977e9b48be
Angelica Patricia Djaya Saputra <121279021+angelpatriciads@users.noreply.github.com>
2023-10-17 18:06:54 +0700

    Add /inhalers end-point to get all data from inhalers table

--- jwt/jwt.js
+++ jwt/jwt.js
@@ -1,42 +1,39 @@
-const {sign, verify} = require('jsonwebtoken');
-require('dotenv').config();
+const { sign, verify } = require("jsonwebtoken");
+require("dotenv").config();
 
 // Create JWT Token
 const createToken = async (user) => {
-
-    const accessToken = sign(
-        {email: user.rows[0].email, user_id: user.rows[0].user_id },
-        process.env.JWT_KEY
-    );
-    console.log(accessToken);
+  const accessToken = sign(
+    { email: user.rows[0].email, user_id: user.rows[0].user_id },
+    process.env.JWT_KEY
+  );
+  console.log(accessToken);
   //  console.log(process.env.JWT_KEY);
-    return accessToken
-}
+  return accessToken;
+};
 
 // Verify token
 const validateToken = async (req, res, next) => {
+  const accessToken = req.cookies["access-token"];
+  const cookie = accessToken.split("=")[1];
 
-    const accessToken = req.cookies["access-token"];
-    const cookie = accessToken.split('=')[1];
+  if (!accessToken) {
+    return res.status(401).json({ error: "User not authenticated" });
+  }
 
-    if (!accessToken){
-        return res.status(401).json({ error: 'User not authenticated' });
+  try {
+    const validToken = await verify(cookie, process.env.JWT_KEY);
+    if (validToken) {
+      req.authenticated = true;
+      return next();
     }
-
-    try {
-        const validToken = await verify(cookie, process.env.JWT_KEY);
-        if (validToken) {
-            req.authenticated = true;
-            return next();
-        } 
-    } catch (error){
-        console.log(error);
-        return res.status(401).json({ error: error.message });
-    }
-
-}
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ error: error.message });
+  }
+};
 
 module.exports = {
-    createToken,
-    validateToken
+  createToken,
+  validateToken,
 };
