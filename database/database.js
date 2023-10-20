fecf7dfeeaddec15f169c784af463b2c6d297283
GeraldyK <76173300+GeraldyK@users.noreply.github.com>
2023-10-20 17:31:56 +0700

    add inhaler_id to puffs

--- database/database.js
+++ database/database.js
@@ -3,21 +3,21 @@ require("dotenv").config();
 const connectionString = process.env.DB_URL;
 
 // connect to database
-//connect to local database
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_DATABASE,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-//   max: 20,
-//   idleTimeoutMillis: 30000,
-//   connectionTimeoutMillis: 2000,
-// });
+connect to local database
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
 
 // connect to remote database
-const pool = new Pool({
-  connectionString,
-});
+// const pool = new Pool({
+//   connectionString,
+// });
 
 module.exports = pool;
