ffba6d87f059181c5671e614650e9c08114d312a
GeraldyK <76173300+GeraldyK@users.noreply.github.com>
2023-10-24 13:36:05 +0700

    update code

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 //middlewate
+
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- jwt/jwt.js
+++ jwt/jwt.js
@@ -3,18 +3,13 @@ require('dotenv').config();
 
 // Create JWT Token
 const createToken = async (user) => {
-
-    const accessToken = sign({
-        user_id: user.rows[0].user_id,
-        inhaler_id: user.rows[0].inhaler_id,
-        email: user.rows[0].email, 
-        password: user.rows[0].password,
-        dob: user.rows[0].dob },
-        process.env.JWT_KEY, 
-        { expiresIn: process.env.JWT_EXPIRES_IN }
+    
+    const accessToken = sign(
+        {email: user.rows[0].email, user_id: user.rows[0].user_id },
+        process.env.JWT_KEY
     );
     // console.log(accessToken);
-  //  console.log(process.env.JWT_KEY);
+    // console.log(process.env.JWT_KEY);
     return accessToken
 }
 
@@ -31,10 +26,7 @@ const validateToken = async (req, res, next) => {
     try {
         console.log('Access Token:', accessToken)
         const validToken = await verify(accessToken, process.env.JWT_KEY);
-        const decode = jwt.decode(accessToken);
-        console.log(decode.payload);
         if (validToken) {
-           // console.log(accessToken.user_id)
             req.authenticated = true;
             return next();
         } 
